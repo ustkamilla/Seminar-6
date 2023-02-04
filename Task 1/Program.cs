0a8a8c5a4dd7328e459ce5956f9e08da9961eaea
Kamilla <ustkamilla@mail.ru>
2023-02-04 15:26:56 +0500

    task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 1: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int InputInt(string message)
+{
+    System.Console.WriteLine($"{message}");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int len)
+{
+    System.Console.WriteLine("Введите числа:");
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int len = InputInt("Сколько чисел вы хотите ввести?");
+int[] array = CreateArray(len);
+System.Console.WriteLine($"Вы ввели {CountPositiveNumbers(array)} числа (чисел) больше 0.");
